b2f0a1aa42bcf47aab0c007eb1a4599e9d323ca3
Yigithan Colak <yigithancolak97@gmail.com>
2023-08-22 09:20:20 +0300

    feat: Deployment test

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(express.json())
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('INFO :: Root route called')
